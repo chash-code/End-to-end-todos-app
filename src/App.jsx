35268cba6c52e8c15fbdf18c9321d13c7d3656db
chash-code <panchalchash@gmail.com>
2026-01-07 23:09:02 +0530

    App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+```javascript
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './routes/ProtectedRoute';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import TodosDashboard from './pages/TodosDashboard';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/todos"
+            element={
+              <ProtectedRoute>
+                <TodosDashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
+```
